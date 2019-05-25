6c0ea8593c599da8c1db3aeb56c6567308ed9c1b
M3chanix <alexei_katitski@mail.ru>
2019-05-25 15:08:43 +0300

    file included in path

--- run_single_segmentation.py
+++ run_single_segmentation.py
@@ -11,8 +11,8 @@ segment = v_seg.v_segmentor(batch_size=1, model='/content/LungLobeSegmentation/m
 
 
 #LOAD THE CT_SCAN
-scan_file = 'test.mhd'
-ct_scan, origin, spacing, orientation = futil.load_itk('/content/LungLobeSegmentation/data/'+scan_file, get_orientation=True)
+#scan_file = 'test.mhd'
+ct_scan, origin, spacing, orientation = futil.load_itk('/content/LungLobeSegmentation/data/test.mhd', get_orientation=True)
 if (orientation[-1] == -1):
     ct_scan = ct_scan[::-1]
 print 'Origem: '
