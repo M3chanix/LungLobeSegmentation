a26341aab8046efa04b8442236486661f893650d
M3chanix <alexei_katitski@mail.ru>
2019-05-27 18:16:22 +0300

    tensorflow memry usage control changed

--- run_single_segmentation.py
+++ run_single_segmentation.py
@@ -2,7 +2,19 @@ import time
 import futils.util as futil
 import segmentor as v_seg
 import keras.backend as K
+import tensorflow as tf
 
+# TensorFlow wizardry
+config = tf.ConfigProto()
+
+# Don't pre-allocate memory; allocate as-needed
+config.gpu_options.allow_growth = True
+
+# Only allow a total of half the GPU memory to be allocated
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+
+# Create a session with the above options specified.
+k.tensorflow_backend.set_session(tf.Session(config=config))
 K.set_learning_phase(1)
 
 #LOAD THE MODEL
